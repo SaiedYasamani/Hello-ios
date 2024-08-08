c8af9091db8dc241ace938241633f6e68ea51ed2
Saeed Yasamani <saeidyasamani@gmail.com>
2024-08-08 08:23:03 +0330

    Initial Commit

---
+++ Hello ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello ios
+//
+//  Created by Saeed Yasamani on 8/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, ios!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello ios/Hello_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_iosApp.swift
+//  Hello ios
+//
+//  Created by Saeed Yasamani on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
